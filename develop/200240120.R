5d7f918358d3ac90b6b61e4e62b4e3ed2782712c
Koji E. Kosugi <kosugitti@gmail.com>
2024-01-21 11:05:04 +0900

    20240121

---
+++ develop/200240120.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+dat <- read_csv("Baseball.csv") %>%
+    filter(Year == "2022年度") %>%
+    dplyr::select(Year, Name, team, salary, position, Games, AtBats, Hit) %>%
+    mutate_at(vars(Games, AtBats, Hit), ~ ifelse(is.na(.), 999, .)) %>%
+    write_csv(file = "Baseball2022.csv")
