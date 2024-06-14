08b337778c11a261333713c4d0e8b61820cfe98e
kosugitti <kosugitti@gmail.com>
2024-06-14 12:54:25 +0900

    2024年 6月14日 金曜日 12時54分24秒 JST

--- compile.sh
+++ compile.sh
@@ -1,20 +1,9 @@
 #!/usr/bin/bash
 source myenv/bin/activate
-# 翻訳
-echo $(date)
-cd develop
-echo '翻訳します'
-python3 TransText.py
-deactivate
-cd ..
+
 # Quarto
 cd jp
 echo '日本語版レンダリングします'
-quarto render
-cd ..
-
-cd en
-echo '英語版レンダリングします'
 quarto render
 cd ..
 
